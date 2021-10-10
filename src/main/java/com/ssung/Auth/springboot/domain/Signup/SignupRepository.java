5db15676986362f451f718148caf880ca44770f0
ssung <qkrtjdcjf124@gmail.com>
2021-10-10 15:14:10 +0900

    회원가입 기능 완료

--- src/main/java/com/ssung/Auth/springboot/domain/Signup/SignupRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ssung.Auth.springboot.domain.Signup;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SignupRepository extends JpaRepository<Signup, Long> {
-}
--- src/main/resources/static/js/app/signup.js
+++ src/main/resources/static/js/app/signup.js
@@ -8,29 +8,27 @@ var main = {
     },
 
     save : function() {
-        var id = $('#id').val();
-        var pw = $('#pw').val();
+        var nickname = $('#nickname').val();
+        var password = $('#password').val();
         var email = $('#email').val();
-        var phone1 = $('#phone1').val();
-        var phone2 = $('#phone2').val();
-        var phone3 = $('#phone3').val();
+        var phone = $('#phone').val();
 
         var data = {
-            id: id,
-            pw: pw,
+            nickname: nickname,
+            password: password,
             email: email,
-            phone: phone1+phone2+phone3
+            phone: phone
         };
 
         $.ajax({
             type: 'POST',
-            url: '/api/signup',
+            url: '/api/s1/member',
             dataType: 'json',
             contentType: 'application/json; charset=utf-8',
             data: JSON.stringify(data)
         }).done(function() {
             alert('회원등록되었습니다.');
-            window.location.href='/main';
+            window.location.href='/';
         }).fail(function() {
             alert(JSON.stringify(error));
         });
